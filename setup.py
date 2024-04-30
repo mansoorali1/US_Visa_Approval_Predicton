283c5a8bafcc6eb456728ca90be9a5959adde77d
mansoorali1 <mansoorali.shaik23@gmail.com>
2024-04-30 19:47:30 +0530

    setup file created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Mansoor",
+    author_email="mansoorali.shaik23@gmail.com",
+    packages=find_packages()
+)
