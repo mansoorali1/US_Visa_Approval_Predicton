b0c565f0af7ae34bec3317cbb8204bea0be3b306
mansoorali1 <mansoorali.shaik23@gmail.com>
2024-05-21 12:17:31 +0530

    app updated

--- app.py
+++ app.py
@@ -118,5 +118,4 @@ async def predictRouteClient(request: Request):
 
 
 if __name__ == "__main__":
-    app_run(app, host=APP_HOST, port=APP_PORT) 
-    
+    app_run(app, host=APP_HOST, port=APP_PORT)
